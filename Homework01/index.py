fb426f6cd9b99bc5b55208fc3e4a6fafeb72e341
pgn2k17 <pigeon2017@hotmail.com>
2018-11-20 20:34:36 +0200

    Homework01

--- Homework01/index.py
+++ Homework01/index.py
@@ -60,14 +60,13 @@ def rpsls(player_choice):
     # convert the player's choice to player_number using the function name_to_number()
     player_number = name_to_number(player_choice)
     # compute random guess for comp_number using random.randrange()
-    comp_number = random.randrange(0,4)
+    comp_number = random.randrange(0,5)
     # convert comp_number to comp_choice using the function number_to_name()
     comp_choice = number_to_name(comp_number)
     # print out the message for computer's choice
     print("Computer chooses " + comp_choice)
     # compute difference of comp_number and player_number modulo five
     difference = (player_number - comp_number)%5
-    print(difference)
     # use if/elif/else to determine winner, print winner message
     if difference == 1 or difference == 2:
         print("Player wins")
@@ -84,3 +83,4 @@ rpsls("lizard")
 rpsls("scissors")
 
 # always remember to check your completed program against the grading rubric
+
